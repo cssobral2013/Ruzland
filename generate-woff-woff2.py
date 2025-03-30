9800ed291fcb1010e8c9aa634a8d0163c036e803
Cristiano Sobral <cssobral2013@gmail.com>
2025-03-29 22:30:26 -0300

    Update version 2.0

---
+++ generate-woff-woff2.py
@@ -0,0 +1,304 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Ruzland-ExtraLight.ttf'
+woffPath = './web/Ruzland-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Ruzland-ExtraLightItalic.ttf'
+woffPath = './web/Ruzland-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Ruzland-Light.ttf'
+woffPath = './web/Ruzland-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-LightItalic.ttf'
+woffPath = './web/Ruzland-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-LightItalic.ttf'
+woffPath = './web/Ruzland-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-Regular.ttf'
+woffPath = './web/Ruzland-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-Italic.ttf'
+woffPath = './web/Ruzland-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-Medium.ttf'
+woffPath = './web/Ruzland-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-MediumItalic.ttf'
+woffPath = './web/Ruzland-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBold.ttf'
+woffPath = './web/Ruzland-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBoldItalic.ttf'
+woffPath = './web/Ruzland-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBold.ttf'
+woffPath = './web/Ruzland-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBoldItalic.ttf'
+woffPath = './web/Ruzland-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-Bold.ttf'
+woffPath = './web/Ruzland-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-BoldItalic.ttf'
+woffPath = './web/Ruzland-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-ExtraBold.ttf'
+woffPath = './web/Ruzland-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-ExtraBoldItalic.ttf'
+woffPath = './web/Ruzland-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+ 
+srcPath  = './ttf/Ruzland-Black.ttf'
+woffPath = './web/Ruzland-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Ruzland-BlackItalic.ttf'
+woffPath = './web/Ruzland-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Ruzland-ExtraLight.ttf'
+woff2Path = './web/Ruzland-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Ruzland-ExtraLightItalic.ttf'
+woff2Path = './web/Ruzland-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Ruzland-Light.ttf'
+woff2Path = './web/Ruzland-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-LightItalic.ttf'
+woff2Path = './web/Ruzland-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-LightItalic.ttf'
+woff2Path = './web/Ruzland-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-Regular.ttf'
+woff2Path = './web/Ruzland-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-Italic.ttf'
+woff2Path = './web/Ruzland-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-Medium.ttf'
+woff2Path = './web/Ruzland-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-MediumItalic.ttf'
+woff2Path = './web/Ruzland-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBold.ttf'
+woff2Path = './web/Ruzland-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBoldItalic.ttf'
+woff2Path = './web/Ruzland-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBold.ttf'
+woff2Path = './web/Ruzland-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-SemiBoldItalic.ttf'
+woff2Path = './web/Ruzland-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-Bold.ttf'
+woff2Path = './web/Ruzland-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-BoldItalic.ttf'
+woff2Path = './web/Ruzland-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-ExtraBold.ttf'
+woff2Path = './web/Ruzland-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-ExtraBoldItalic.ttf'
+woff2Path = './web/Ruzland-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+srcPath  = './ttf/Ruzland-Black.ttf'
+woff2Path = './web/Ruzland-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Ruzland-BlackItalic.ttf'
+woff2Path = './web/Ruzland-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path) 
